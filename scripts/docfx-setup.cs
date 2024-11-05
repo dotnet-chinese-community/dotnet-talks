974f0f2be866c06b6acaf0714fb4fcb426f7e5bf
Weihan Li <weihanli@outlook.com>
2024-11-05 08:43:25 +0800

    update docfx setup
    [skip ci]

--- scripts/docfx-setup.cs
+++ scripts/docfx-setup.cs
@@ -7,37 +7,35 @@ const string tocFormat = """
 string[] folders = ["./2024"];
 
 ReadmeToIndex(Environment.CurrentDirectory);
-
 var rootTocPath = "./toc.yml";
 if (!File.Exists(rootTocPath))
 {
-	var navLines = folders.Select(dir => string.Format(tocFormat, new DirectoryInfo(dir).Name));
-	await File.WriteAllLinesAsync(rootTocPath, navLines, Encoding.UTF8);
+    var navLines = folders.Select(dir => string.Format(tocFormat, new DirectoryInfo(dir).Name));
+    await File.WriteAllLinesAsync(rootTocPath, navLines, Encoding.UTF8);
 }
 
 foreach(var folder in folders)
 {
-	ReadmeToIndex(folder);
-	var dirList = new List<string>();
-	foreach (var dir in Directory.GetDirectories(folder))
-	{
-		dirList.Add(new DirectoryInfo(dir).Name);
-		ReadmeToIndex(dir);
+    ReadmeToIndex(folder);
+    var dirList = new List<string>();
+    foreach (var dir in Directory.GetDirectories(folder))
+    {
+        dirList.Add(new DirectoryInfo(dir).Name);
+        ReadmeToIndex(dir);
 	}
-	// generate toc.yml
-	var tocPath = Path.Combine(folder, "toc.yml");
-	var lines = dirList.Order().Select(dir => string.Format(tocFormat, dir));	
-	await File.WriteAllLinesAsync(tocPath, lines, Encoding.UTF8);
-	Console.WriteLine(File.ReadAllText(tocPath));
+    // generate toc.yml
+    var tocPath = Path.Combine(folder, "toc.yml");
+    var lines = dirList.Order().Select(dir => string.Format(tocFormat, dir));	
+    await File.WriteAllLinesAsync(tocPath, lines, Encoding.UTF8);
 }
 
 // replace READ.md => index.md
 static void ReadmeToIndex(string folder)
 {
-	var readmePath = Path.Combine(folder, "README.md");
-	var indexPath = Path.Combine(folder, "index.md");
-	if (File.Exists(readmePath) && !File.Exists(indexPath))
-	{
-		File.Move(readmePath, indexPath);
-	}
+    var readmePath = Path.Combine(folder, "README.md");
+    var indexPath = Path.Combine(folder, "index.md");
+    if (File.Exists(readmePath) && !File.Exists(indexPath))
+    {
+        File.Move(readmePath, indexPath);
+    }
 }
